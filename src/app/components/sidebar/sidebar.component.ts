2dec71e0b1c96ab90bbfe1949335f1f034b3f2d3
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-10-07 03:21:17 +0200

    made more changes relating to the sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonEngine } from '@angular/ssr';
 
 @Component({
   selector: 'nat-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  public open: boolean = true;
+
+  ngOnInit(): void {
+
+  }
+
+  public open_close(): void {
+    // will do the reverse currently active:
+    this.open = !this.open;
+  }
 }
