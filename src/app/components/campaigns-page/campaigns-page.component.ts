d18dc43ec86dd4f02f3efb7f962056f227e4703a
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-11-23 07:13:27 -0500

    added the campaigns page component

---
+++ src/app/components/campaigns-page/campaigns-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nat-campaigns-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-page.component.html',
+  styleUrl: './campaigns-page.component.scss'
+})
+export class CampaignsPageComponent {
+
+}
