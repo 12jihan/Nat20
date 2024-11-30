8b62d2f3ac95bdb1dcd9ba29f5301e8bc6412786
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-11-30 15:21:26 -0500

    added a searching function... not sure how this would work honestly but it'll be touch and go

---
+++ src/app/components/full-search/full-search.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FullSearchComponent } from './full-search.component';
+
+describe('FullSearchComponent', () => {
+  let component: FullSearchComponent;
+  let fixture: ComponentFixture<FullSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FullSearchComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FullSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/full-search/full-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nat-full-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './full-search.component.html',
+  styleUrl: './full-search.component.scss'
+})
+export class FullSearchComponent {
+
+}
